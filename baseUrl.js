2d78566b059ef2b4be2df4e249f9c10f38b068cf
jagh06 <jhonguzmanhernandez659@gmail.com>
2023-12-07 07:02:33 -0600

    agregado de estilos

--- baseUrl.js
+++ baseUrl.js
@@ -1,4 +1,4 @@
-//const baseURL = "https://api-turingo.onrender.com/"
-const baseURL =  "http://localhost:3001/"
+const baseURL = "https://api-turingo.onrender.com/"
+//const baseURL =  "http://localhost:3001/"
 
 export {baseURL}
